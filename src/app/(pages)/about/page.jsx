b25e4e9f167747f7c5fe5ba9edfa95af81f4084e
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2023-10-09 21:37:10 +0600

    time functionality

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const about = async () => {
 
-  const res = await fetch("https://jsonplaceholder.typicode.com/users")
-  const user = await res.json()
-
+const res = await fetch("https://jsonplaceholder.typicode.com/users",{cache: 'no-store'})
+const user = await res.json()
 
 
   return (
     <section>
       <div className="container">
+        <h1>{new Date().toLocaleTimeString()}</h1>
           <h1 className='about_heading'>About page Data Fetching</h1>
           <div className="about_wrapper">
             {
@@ -17,6 +17,7 @@ const about = async () => {
                 <div className="about_item">
                   <h3>Name: {item.name}</h3>
                   <p>Email: {item.email}</p>
+                  <p>Phone: {item.phone}</p>
                 </div>
               )
             }
